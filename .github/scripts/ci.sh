c0f30b09e8e96ea6627e3410a09e7c189bb55c99
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2022-08-13 16:50:50 +0900

    chore: Update ci.sh

--- .github/scripts/ci.sh
+++ .github/scripts/ci.sh
@@ -1,1 +1,9 @@
-
+if [ -n $(git log --format=%s $(git describe --tags)..HEAD) ]
+then
+  content=`cat ./package.json`
+  content="${content//'%'/'%25'}"
+  content="${content//$'\n'/'%0A'}"
+  content="${content//$'\r'/'%0D'}"
+  
+  echo $content
+fi
