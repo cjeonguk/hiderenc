85fd68f921b27eda0fdf112cfb6b4e3461d8a95c
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2022-08-16 08:57:49 +0900

    fix(encrypt): Fix .odt encoding problem
    Use crypto-js instead of crypto

--- src/main/modules/encrypt.js
+++ src/main/modules/encrypt.js
@@ -1,26 +1,19 @@
 "use strict";
 exports.__esModule = true;
 exports.decryptFile = exports.encryptFile = exports.decrypt = exports.encrypt = void 0;
+var CryptoJS = require("crypto-js");
 var crypto = require("crypto");
 var fs_1 = require("fs");
 var path_1 = require("path");
 var encrypt = function (input, password) {
-    var salt = crypto.randomBytes(16);
-    var key = crypto.scryptSync(password, salt, 32);
-    var iv = crypto.randomBytes(16);
-    var cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    return "".concat(iv.toString('hex'), ":").concat(salt.toString('hex'), ":").concat(cipher.update(input, 'utf8', 'hex') + cipher.final('hex'));
+    var ciphertext = CryptoJS.AES.encrypt(input, password).toString();
+    return ciphertext;
 };
 exports.encrypt = encrypt;
 var decrypt = function (input, password, randomStr) {
-    var texts = input.split(':');
-    var iv = Buffer.from(texts[0], 'hex');
-    var salt = Buffer.from(texts[1], 'hex');
-    var text = texts[2];
-    var key = crypto.scryptSync(password, salt, 32);
-    var decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     try {
-        return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+        var deciphertext = CryptoJS.AES.decrypt(input, password).toString(CryptoJS.enc.Utf8);
+        return deciphertext;
     }
     catch (err) {
         return randomStr;
@@ -28,17 +21,17 @@ var decrypt = function (input, password, randomStr) {
 };
 exports.decrypt = decrypt;
 var encryptFile = function (filePath, password) {
-    var content = (0, fs_1.readFileSync)(filePath, { encoding: 'utf8' });
+    var content = (0, fs_1.readFileSync)(filePath, 'base64');
     var encryptedContent = (0, exports.encrypt)(content, password);
-    (0, fs_1.writeFileSync)(filePath + '.enc', encryptedContent);
+    (0, fs_1.writeFileSync)(filePath + '.enc', encryptedContent, 'base64');
 };
 exports.encryptFile = encryptFile;
 var decryptFile = function (filePath, password) {
     var randomStr = (crypto.randomInt(256) * crypto.randomInt(256)).toString();
-    var content = (0, fs_1.readFileSync)(filePath, { encoding: 'utf8' });
+    var content = (0, fs_1.readFileSync)(filePath, 'base64');
     var decryptedContent = (0, exports.decrypt)(content, password, randomStr);
     if (decryptedContent !== randomStr) {
-        (0, fs_1.writeFileSync)((0, path_1.resolve)((0, path_1.dirname)(filePath), (0, path_1.basename)(filePath, (0, path_1.extname)(filePath))), decryptedContent);
+        (0, fs_1.writeFileSync)((0, path_1.resolve)((0, path_1.dirname)(filePath), (0, path_1.basename)(filePath, (0, path_1.extname)(filePath))), decryptedContent, 'base64');
         return true;
     }
     else
--- src/main/modules/encrypt.ts
+++ src/main/modules/encrypt.ts
@@ -1,18 +1,11 @@
+import * as CryptoJS from 'crypto-js';
 import * as crypto from 'crypto';
 import { readFileSync, writeFileSync } from 'fs';
 import { basename, dirname, resolve, extname } from 'path';
 
 export const encrypt = (input: string, password: string) => {
-  const salt = crypto.randomBytes(16);
-  const key = crypto.scryptSync(password, salt, 32);
-
-  const iv = crypto.randomBytes(16);
-
-  const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-
-  return `${iv.toString('hex')}:${salt.toString('hex')}:${
-    cipher.update(input, 'utf8', 'hex') + cipher.final('hex')
-  }`;
+  const ciphertext = CryptoJS.AES.encrypt(input, password).toString();
+  return ciphertext;
 };
 
 export const decrypt = (
@@ -20,36 +13,31 @@ export const decrypt = (
   password: string,
   randomStr: string
 ): string => {
-  const texts = input.split(':');
-  const iv = Buffer.from(texts[0], 'hex');
-  const salt = Buffer.from(texts[1], 'hex');
-  const text = texts[2];
-
-  const key = crypto.scryptSync(password, salt, 32);
-
-  const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-
   try {
-    return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+    const deciphertext = CryptoJS.AES.decrypt(input, password).toString(
+      CryptoJS.enc.Utf8
+    );
+    return deciphertext;
   } catch (err) {
     return randomStr;
   }
 };
 
 export const encryptFile = (filePath: string, password: string) => {
-  const content = readFileSync(filePath, { encoding: 'utf8' });
+  const content = readFileSync(filePath, 'base64');
   const encryptedContent = encrypt(content, password);
-  writeFileSync(filePath + '.enc', encryptedContent);
+  writeFileSync(filePath + '.enc', encryptedContent, 'base64');
 };
 
 export const decryptFile = (filePath: string, password: string) => {
   const randomStr = (crypto.randomInt(256) * crypto.randomInt(256)).toString();
-  const content = readFileSync(filePath, { encoding: 'utf8' });
+  const content = readFileSync(filePath, 'base64');
   const decryptedContent = decrypt(content, password, randomStr);
   if (decryptedContent !== randomStr) {
     writeFileSync(
       resolve(dirname(filePath), basename(filePath, extname(filePath))),
-      decryptedContent
+      decryptedContent,
+      'base64'
     );
     return true;
   } else return false;
