381f1ac07c727c2b83a530d31da660be1ac01938
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2022-08-15 20:27:17 +0900

    chore: Fix ci.sh

--- .github/scripts/ci.sh
+++ .github/scripts/ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 content="$(git log --format=%s $(git describe --tags --always --abbrev=0)..HEAD)"
+echo $content
 if [[ -z $content ]]
 then
   echo "Nothing changed"
@@ -11,27 +12,17 @@ else
   git config user.name "github-actions[bot]"
   git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 
-  # version=`git describe --tags --abbrev=0`
-  # version="${version:1}"
-  # echo $version
-
   changelog="$(echo -e "# $(date +'%Y-%m-%d')\n$content" | sed -e "s/^/\* /")"
   changelog="${changelog:3}"
   echo -e $changelog | cat - CHANGELOG.md > temp && mv temp CHANGELOG.md
   git add CHANGELOG.md
 
   version="$(node -e "console.log(require('./package.json').version);")"
-  if [[ $version == *".0.0" ]]
+  if [[ $content == *"feat("*"):"* ]]
   then
-    git commit -m "chore: Publish v$version"
-    git tag -a v$version -m "v$version"
+    yarn version --minor
   else
-    if [[ $content == *"feat("*"):"* ]]
-    then
-      yarn version --minor
-    else
-      yarn version --patch
-    fi
+    yarn version --patch
   fi
   git push && git push --tags
 
@@ -40,5 +31,5 @@ else
   yarn dist
   yarn dist:mac-arm64
 
-  gh release create v$version --notes $changelog "./dist/hider-v$version-win-x64.exe" "./dist/hider-v$version-linux-x64.zip" "./dist/hider-v$version-mac-x64.dmg" "./dist/hider-v$version-mac-arm64.dmg"
+  gh release create v$version --notes "$changelog" "dist/hider-v$version-win-x64.exe" "dist/hider-v$version-linux-x64.zip" "dist/hider-v$version-mac-x64.dmg" "dist/hider-v$version-mac-arm64.dmg"
 fi
