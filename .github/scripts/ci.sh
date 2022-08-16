8cc48ebce355c2cd6d6dc0bcb75e3b6c5be6641f
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2022-08-16 09:10:32 +0900

    chore: Update ci.sh about changelog

--- .github/scripts/ci.sh
+++ .github/scripts/ci.sh
@@ -12,7 +12,6 @@ else
   git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 
   changelog="$(echo -e "# $(date +'%Y-%m-%d')\n\n$(git log --oneline --decorate $(git describe --tags --abbrev=0)..HEAD | sed -e "s/^/\* /")")"
-  changelog="${changelog:2}"
   echo -e "$changelog\n\n$(cat CHANGELOG.md)" > CHANGELOG.md
   git add CHANGELOG.md
   git commit -m "chore: Write changelog"
