e86762c5662b466e419a7ebdde57053dca38f494
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2022-08-21 22:41:31 +0900

    chore(ci): Add latest.yml to release

--- .github/scripts/ci.sh
+++ .github/scripts/ci.sh
@@ -21,10 +21,10 @@ else
 
   git push
 
-  mkdir app
   yarn build
-  yarn dist
-  yarn dist:mac-arm64
+  yarn dist:win
+  yarn dist:linux
+  yarn dist:mac:both
 
-  gh release create v$version --notes "$(echo -e $changelog)" "dist/hider-v$version-win-x64.exe" "dist/hider-v$version-linux-x64.zip" "dist/hider-v$version-mac-x64.dmg" "dist/hider-v$version-mac-arm64.dmg"
+  gh release create v$version --notes "$(echo -e $changelog)" "dist/hider-v$version-win-x64.exe" "dist/hider-v$version-linux-x64.zip" "dist/hider-v$version-mac-x64.dmg" "dist/hider-v$version-mac-arm64.dmg" "dist/latest.yml" "dist/latest-mac.yml"
 fi
