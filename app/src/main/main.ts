13a6061017893b37f891c3762515922e4a0743d9
Jeonguk Choi <91456421+cjeonguk@users.noreply.github.com>
2022-09-04 18:23:54 +0900

    fix(main): Quit program after update downloaded

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -70,7 +70,7 @@ autoUpdater.on('update-available', () => {
     message: 'Available updates exist.',
     title: 'Update',
     type: 'question',
-    buttons: ['Not now', 'Download update'],
+    buttons: ['Not now', 'Quit the program and update'],
     defaultId: 1,
   };
 
@@ -84,11 +84,7 @@ autoUpdater.on('download-progress', (progressObj) => {
 
 autoUpdater.on('update-downloaded', () => {
   mainWindow.setProgressBar(-1);
-  dialog.showMessageBox(mainWindow, {
-    title: 'Notice',
-    message: 'Update downloaded.',
-    type: 'info',
-  });
+  autoUpdater.quitAndInstall();
 });
 
 autoUpdater.on('error', (err) => {
